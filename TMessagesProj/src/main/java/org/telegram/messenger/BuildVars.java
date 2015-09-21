e0161179345894b757c1a9af45e7fe25c8586f4a
drakeet <drakeet.me@gmail.com>
2015-09-21 14:25:37 +0800

    Make public of my BuildVars.

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,23 @@
+/*
+ * This is the source code of Telegram for Android v. 2.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2015.
+ */
+
+package org.telegram.messenger;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static int BUILD_VERSION = 592;
+    public static int APP_ID = 23630; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "522b7ac779a2009c9c5aa555951bd1d6"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "66346190bd46042c289bee55308acfef";
+    public static String GCM_SENDER_ID = "760348033672";
+    public static String SEND_LOGS_EMAIL = "drakeet.me@gmail.com";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_VERSION = "20150326";
+}
